bb67f8bb8bcaa5a7fa265c4b4cf098087d7d76e9
SuicideCatt <random.cat.from.internet@gmail.com>
2025-01-04 03:48:59 +0200

    test: add testing

---
+++ tests/add_new_parameter.cpp
@@ -0,0 +1,75 @@
+#include <ArgsP.hpp>
+#include <iostream>
+
+namespace ArgsP = SCT::ArgsP;
+
+struct Resolution
+{
+    int width, height;
+};
+
+ArgsP::Error::Code
+	parse_resolution(Resolution& value, std::string_view new_value)
+{
+	auto i = new_value.find('x');
+	if (i == new_value.npos)
+		return ArgsP::Error::Code::syntax_error;
+
+	auto width =
+		ArgsP::str_to<uint32_t>(new_value.substr(0, i));
+	if (!width.has_value())
+		return width.error();
+
+	auto height =
+		ArgsP::str_to<uint32_t>(new_value.substr(i + 1));
+	if (!height)
+		return height.error();
+
+	value.width = width.value_or();
+	value.height = height.value_or();
+
+	return ArgsP::Error::Code::no_error;
+}
+
+using ResolutionParameter =
+	ArgsP::Base::Parameter<Resolution, &parse_resolution>;
+using ResolutionArgument = ArgsP::Base::Argument<Resolution, &parse_resolution>;
+
+
+struct AltResolutionParameter : ArgsP::Base::Parameter<Resolution>
+{
+	AltResolutionParameter(Infos names, Resolution base = {100, 100})
+		: ArgsP::Base::Parameter<Resolution>(names, base) {}
+
+	ArgsP::Error::Code parse_value(std::string_view new_value) override
+		{ return parse_resolution(p_value, new_value); }
+};
+
+int main(int argc, char** argv)
+{
+	ArgsP::Parser p('-', false);
+
+	ResolutionParameter res({{1, "r"}, {2, "res"}});
+	AltResolutionParameter alt_res({{1, "ar"}, {2, "alt_res"}});
+	p.add_arguments(res, alt_res);
+
+	auto program = p.parse(argc, argv);
+	if (p.error())
+	{
+		auto& err = p.get_error_container().get_error();
+
+		std::cerr << err.what() << '\n';
+
+		return 1;
+	}
+
+	const auto& res_ref = *res;
+	const auto& alt_res_ref = *alt_res;
+	std::cout << *program << ":\n";
+	std::cout << "  res(WxH): " << res_ref.width << 'x'
+			  << res_ref.height << '\n';
+	std::cout << "  alt_res(WxH): " << alt_res_ref.width << 'x'
+			  << alt_res_ref.height << '\n';
+
+	return 0;
+}
---
+++ tests/main.cpp
@@ -0,0 +1,64 @@
+#include <ArgsP.hpp>
+#include <Error.hpp>
+
+#include <iostream>
+#include <iomanip>
+
+int main(int argc, char** argv)
+{
+	auto t = [](const SCT::ArgsP::ArgV& argv)
+	{
+		using namespace SCT::ArgsP;
+
+		Flag::Bool help({{1, "h"}, {2, "help"}});
+		Arguments::String path;
+		Parameters::Int64 res_w({{1, "w"}, {2, "res_w"}}, 100);
+		Parameters::Int64 res_h({{1, "h"}, {2, "res_h"}}, 100);
+
+		Parser p('-', false);
+
+		p.add_arguments(help, path, res_w, res_h);
+
+		for (const auto& arg : argv)
+			std::cout << arg << ' ';
+		std::cout << '\n';
+
+		auto program = p.parse(argv);
+		if (p.error())
+		{
+			const auto& error = p.get_error_container().get_error();
+			std::cout << "what(): " << error.what() << '\n';
+
+			if (error.argument.size())
+				std::cout << error.argument << '=';
+			std::cout << std::quoted(error.value);
+			std::cout << ": " << error.code().message() << '\n';
+
+			std::cout << '\n';
+			return;
+		}
+
+		std::cout << *program << ":\n";
+		std::cout << "  help(bool):   " << *help << '\n';
+		std::cout << "  path(string): " << std::quoted(*path) << '\n';
+		std::cout << "  res_w(int):   " << *res_w << '\n';
+		std::cout << "  res_h(int):   " << *res_h << '\n';
+
+		std::cout << '\n';
+	};
+
+	t({"test1", "-h", "-w=1z2", "/path", "-h", "120"});
+	t({"test2", "--help", "--res_w=12", "/path", "--res_h", "120"});
+	t({"test3", "--help", "/path", "--res_h", "120"});
+	t({"test4", "-w", "120"});
+
+	t({"test5", "-h=2", "123"}); // error test
+	t({"test6", "-h", "-w=123.f"}); // error test
+
+	t({"test7", "-w", "0b011"}); // binary!
+	t({"test8", "-w", "0xF"}); // hex!
+
+	new int[10];
+
+	return 0;
+}
