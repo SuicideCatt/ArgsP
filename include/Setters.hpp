ca1d344d0755bbc7fff1375770bd763079b0f3b3
SuicideCatt <random.cat.from.internet@gmail.com>
2025-07-18 19:52:41 +0300

    setters: add int/uint support, add floating support

--- include/Setters.hpp
+++ include/Setters.hpp
@@ -10,7 +10,7 @@
 
 namespace SCT::ArgsP::Setters
 {
-	enum class IntBase
+	enum class IntBase : int
 	{
 		automatic = 0, bin = 2, dec = 10, hex = 16
 	};
@@ -19,8 +19,11 @@ namespace SCT::ArgsP::Setters
 		Error::Code parse(std::string_view& value, std::string_view new_value);
 	SCT_ArgsP_INL Error::Code parse(bool& value, std::string_view new_value);
 
-	template<IntBase base = IntBase::automatic>
-	SCT_ArgsP_INL Error::Code parse(int64_t& value, std::string_view new_value);
+	template<std::integral Type, IntBase base = IntBase::automatic>
+	SCT_ArgsP_INL Error::Code parse(Type& value, std::string_view new_value);
+
+	template<std::floating_point Type>
+	SCT_ArgsP_INL Error::Code parse(Type& value, std::string_view new_value);
 }
 
 #include "ipp/Setters.hpp"
--- include/tpp/Setters.hpp
+++ include/tpp/Setters.hpp
@@ -4,26 +4,32 @@
 
 namespace SCT::ArgsP::Setters
 {
-	template<>
-	SCT_ArgsP_INL Error::Code
-		parse<IntBase::automatic>(int64_t& v, std::string_view new_value)
+	template<std::integral Type, IntBase base>
+	SCT_ArgsP_INL Error::Code parse(Type& v, std::string_view new_value)
 	{
 		int b = 10;
-		if (new_value.size() > 1 && new_value[0] == '0')
+		if constexpr (base == IntBase::automatic)
 		{
-			if ((new_value[1] == 'b' || new_value[1] == 'B'))
+			if (new_value.size() > 1 && new_value[0] == '0')
 			{
-				new_value = new_value.substr(2);
-				b = 2;
-			}
-			else if ((new_value[1] == 'x' || new_value[1] == 'X'))
-			{
-				new_value = new_value.substr(2);
-				b = 16;
+				if ((new_value[1] == 'b' || new_value[1] == 'B'))
+				{
+					new_value = new_value.substr(2);
+					b = 2;
+				}
+				else if ((new_value[1] == 'x' || new_value[1] == 'X'))
+				{
+					new_value = new_value.substr(2);
+					b = 16;
+				}
 			}
 		}
+		else
+		{
+			b = static_cast<int>(base);
+		}
 
-		auto res = str_to<int64_t>(new_value, nullptr, b);
+		auto res = str_to<Type>(new_value, nullptr, b);
 		if (res)
 		{
 			v = res.value_or();
@@ -33,10 +39,10 @@ namespace SCT::ArgsP::Setters
 		return res.error();
 	}
 
-	template<IntBase base>
-	SCT_ArgsP_INL Error::Code parse(int64_t& v, std::string_view new_value)
+	template<std::floating_point Type>
+	SCT_ArgsP_INL Error::Code parse(Type& v, std::string_view new_value)
 	{
-		auto res = str_to<int64_t>(new_value, nullptr, static_cast<int>(base));
+		auto res = str_to<Type>(new_value);
 		if (res)
 		{
 			v = res.value_or();
