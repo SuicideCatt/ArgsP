4258089613e6451a582e3e9ba63235801a4129cc
SuicideCatt <random.cat.from.internet@gmail.com>
2025-07-18 18:17:24 +0300

    setters/int: fix: single-character number error

--- include/tpp/Setters.hpp
+++ include/tpp/Setters.hpp
@@ -8,24 +8,22 @@ namespace SCT::ArgsP::Setters
 	SCT_ArgsP_INL Error::Code
 		parse<IntBase::automatic>(int64_t& v, std::string_view new_value)
 	{
-		std::string_view w_nv;
 		int b = 10;
-		if ((new_value[1] == 'b' || new_value[1] == 'B') && new_value[0] == '0')
+		if (new_value.size() > 1 && new_value[0] == '0')
 		{
-			w_nv = new_value.substr(2);
-			b = 2;
-		}
-		else if ((new_value[1] == 'x' || new_value[1] == 'X') && new_value[0] == '0')
-		{
-			w_nv = new_value.substr(2);
-			b = 16;
-		}
-		else
-		{
-			w_nv = new_value;
+			if ((new_value[1] == 'b' || new_value[1] == 'B'))
+			{
+				new_value = new_value.substr(2);
+				b = 2;
+			}
+			else if ((new_value[1] == 'x' || new_value[1] == 'X'))
+			{
+				new_value = new_value.substr(2);
+				b = 16;
+			}
 		}
 
-		auto res = str_to<int64_t>(w_nv, nullptr, b);
+		auto res = str_to<int64_t>(new_value, nullptr, b);
 		if (res)
 		{
 			v = res.value_or();
