9924836ddbc915272700ccab6185937ba9561a29
SuicideCatt <random.cat.from.internet@gmail.com>
2025-09-18 02:19:34 +0300

    Base/Argument: update for use std::optional

--- include/Base/Argument.hpp
+++ include/Base/Argument.hpp
@@ -3,29 +3,62 @@
 #include "../Error.hpp"
 #include "Base.hpp"
 
+#include <optional>
+
 namespace SCT::ArgsP::Base
 {
 	template<typename Type>
 	using ValueParsePtr = Error::Code(*)(Type&, std::string_view);
 
 	template<typename _Type, ValueParsePtr<_Type> parse_function = nullptr>
-	class Argument : public Base
+	class Argument;
+
+	template<typename _Type, ValueParsePtr<std::optional<_Type>> parse_function>
+	class Argument<std::optional<_Type>, parse_function> : public Base
 	{
 	public:
 		using Type = _Type;
+		using OptType = std::optional<Type>;
 
-		SCT_ArgsP_INL Argument(const Type& base_value = Type());
+		SCT_ArgsP_INL Argument(OptType base_value = std::nullopt);
 		virtual ~Argument() override = default;
 
-		SCT_ArgsP_INL const Type& get() const noexcept;
-		SCT_ArgsP_INL operator const Type&() const noexcept;
-		SCT_ArgsP_INL const Type& operator*() const noexcept;
+		SCT_ArgsP_INL const OptType& get() const noexcept;
+
+		SCT_ArgsP_INL bool has_value() const noexcept;
+		SCT_ArgsP_INL operator bool() const noexcept;
+
+		SCT_ArgsP_INL Type value_or(Type&& default_value) const noexcept;
+
+		SCT_ArgsP_INL const Type& operator*();
+		SCT_ArgsP_INL const Type& value();
 
 	protected:
 		virtual bool parse(SCT_ArgsP_parse_parameters) override;
 		virtual Error::Code parse_value(std::string_view new_value);
 
-	 	Type p_value;
+		OptType p_value;
+	};
+
+	template<ValueParsePtr<bool> parse_function>
+	class Argument<bool, parse_function> : public Base
+	{
+	public:
+		using Type = bool;
+
+		SCT_ArgsP_INL Argument(bool base_value = false);
+		virtual ~Argument() override = default;
+
+		SCT_ArgsP_INL bool get() const noexcept;
+		SCT_ArgsP_INL operator bool() const noexcept;
+		SCT_ArgsP_INL bool operator*() const noexcept;
+		SCT_ArgsP_INL bool value() const noexcept;
+
+	protected:
+		virtual bool parse(SCT_ArgsP_parse_parameters) override;
+		virtual Error::Code parse_value(std::string_view new_value);
+
+	 	bool p_value;
 	};
 }
 
--- include/tpp/Base/Argument.hpp
+++ include/tpp/Base/Argument.hpp
@@ -4,26 +4,43 @@
 
 namespace SCT::ArgsP::Base
 {
-	template<typename T, ValueParsePtr<T> parse_function>
-	SCT_ArgsP_INL Argument<T, parse_function>::Argument(const T& base_value)
+	template<typename Type, ValueParsePtr<std::optional<Type>> parse_function>
+	SCT_ArgsP_INL Argument<std::optional<Type>, parse_function>
+		::Argument(OptType base_value)
 		: p_value(base_value) {}
 
-	template<typename T, ValueParsePtr<T> parse_function>
-	SCT_ArgsP_INL const T& Argument<T, parse_function>::get() const noexcept
+	template<typename Type, ValueParsePtr<std::optional<Type>> parse_function>
+	SCT_ArgsP_INL auto Argument<std::optional<Type>, parse_function>::get() const noexcept
+		-> const OptType&
 		{ return p_value; }
 
-	template<typename T, ValueParsePtr<T> parse_function>
-	SCT_ArgsP_INL
-		Argument<T, parse_function>::operator const T&() const noexcept
-		{ return get(); }
+	template<typename Type, ValueParsePtr<std::optional<Type>> parse_function>
+	SCT_ArgsP_INL bool Argument<std::optional<Type>, parse_function>
+		::has_value() const noexcept
+		{ return p_value.has_value(); }
 
-	template<typename T, ValueParsePtr<T> parse_function>
-	SCT_ArgsP_INL
-		const T& Argument<T, parse_function>::operator*() const noexcept
-		{ return get(); }
+	template<typename Type, ValueParsePtr<std::optional<Type>> parse_function>
+	SCT_ArgsP_INL Argument<std::optional<Type>, parse_function>
+		::operator bool() const noexcept
+		{ return p_value; }
 
-	template<typename T, ValueParsePtr<T> parse_function>
-	bool Argument<T, parse_function>::parse(SCT_ArgsP_parse_parameters)
+	template<typename Type, ValueParsePtr<std::optional<Type>> parse_function>
+	SCT_ArgsP_INL Type Argument<std::optional<Type>, parse_function>
+		::value_or(Type&& default_value) const noexcept
+		{ return p_value.value_or(std::forward<Type>(default_value)); }
+
+	template<typename Type, ValueParsePtr<std::optional<Type>> parse_function>
+	SCT_ArgsP_INL const Type& Argument<std::optional<Type>, parse_function>
+		::operator*()
+		{ return p_value.value(); }
+
+	template<typename Type, ValueParsePtr<std::optional<Type>> parse_function>
+	SCT_ArgsP_INL const Type& Argument<std::optional<Type>, parse_function>
+		::value()
+		{ return p_value.value(); }
+
+	template<typename Type, ValueParsePtr<std::optional<Type>> parse_function>
+	bool Argument<std::optional<Type>, parse_function>::parse(SCT_ArgsP_parse_parameters)
 	{
 		using namespace Error;
 
@@ -40,9 +57,57 @@ namespace SCT::ArgsP::Base
 		return false;
 	}
 
-	template<typename T, ValueParsePtr<T> parse_function>
+	template<typename Type, ValueParsePtr<std::optional<Type>> parse_function>
+	SCT_ArgsP_INL Error::Code Argument<std::optional<Type>, parse_function>
+		::parse_value(std::string_view new_value)
+	{
+		if constexpr (parse_function == nullptr)
+			return Error::Code::no_function_for_parse_value;
+		else
+			return parse_function(this->p_value, new_value);
+	}
+
+	template<ValueParsePtr<bool> parse_function>
+	SCT_ArgsP_INL Argument<bool, parse_function>::Argument(bool base_value)
+		: p_value(base_value) {}
+
+	template<ValueParsePtr<bool> parse_function>
+	SCT_ArgsP_INL bool Argument<bool, parse_function>::get() const noexcept
+		{ return p_value; }
+
+	template<ValueParsePtr<bool> parse_function>
+	SCT_ArgsP_INL Argument<bool, parse_function>::operator bool() const noexcept
+		{ return p_value; }
+
+	template<ValueParsePtr<bool> parse_function>
+	SCT_ArgsP_INL bool Argument<bool, parse_function>::operator*() const noexcept
+		{ return p_value; }
+
+	template<ValueParsePtr<bool> parse_function>
+	SCT_ArgsP_INL bool Argument<bool, parse_function>::value() const noexcept
+		{ return p_value; }
+
+	template<ValueParsePtr<bool> parse_function>
+	bool Argument<bool, parse_function>::parse(SCT_ArgsP_parse_parameters)
+	{
+		using namespace Error;
+
+		const auto& value = *iterator;
+		if ((*iterator)[0] != prefix)
+		{
+			auto err = parse_value(value);
+			if (err == Code::no_error)
+				return true;
+
+			error.set_error(Exception(std::string(value), err));
+		}
+
+		return false;
+	}
+
+	template<ValueParsePtr<bool> parse_function>
 	SCT_ArgsP_INL Error::Code
-		Argument<T, parse_function>::parse_value(std::string_view new_value)
+		Argument<bool, parse_function>::parse_value(std::string_view new_value)
 	{
 		if constexpr (parse_function == nullptr)
 			return Error::Code::no_function_for_parse_value;
